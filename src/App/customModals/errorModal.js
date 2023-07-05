d770eb5aa5041ce33f1093112c20babad1873da2
Asad Mumtaz <asadmumtaz92@gmail.com>
2023-07-05 17:30:44 +0500

    This commit for add ReactDOM Portal...

--- src/App/customModals/errorModal.js
+++ src/App/customModals/errorModal.js
@@ -1,27 +1,50 @@
 import React from "react";
 
 import styles from './styles/errorModal.module.css'
+import ReactDOM from 'react-dom';
+
+const Backdrop = (props) => {
+    return <div className={styles.backdrop} onClick={props.onClick} />;
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={styles.modal}>
+            {/* HEADER */}
+            <header className={styles.header}>
+                <h2>{props?.title}</h2>
+            </header>
+
+            {/* BODY */}
+            <div className={styles.content}>
+                <p>{props?.message}</p>
+            </div>
+
+            {/* FOOTER */}
+            <footer className={styles.actions}>
+                <button className={styles.button} onClick={props?.onClick}>okay</button>
+            </footer>
+        </div>
+    )
+};
 
 const ErrorModal = ({ title, message, buttonHandler }) => {
     return (
-        <div className={styles.backdrop} onClick={buttonHandler}>
-            <div className={styles.modal}>
-                {/* HEADER */}
-                <header className={styles.header}>
-                    <h2>{title}</h2>
-                </header>
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop onClick={buttonHandler} />,
+                document.getElementById('backdrop-root')
+            )}
 
-                {/* BODY */}
-                <div className={styles.content}>
-                    <p>{message}</p>
-                </div>
-
-                {/* FOOTER */}
-                <footer className={styles.actions}>
-                    <button className={styles.button} onClick={buttonHandler}>okay</button>
-                </footer>
-            </div>
-        </div>
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={title}
+                    message={message}
+                    onClick={buttonHandler}
+                />,
+                document.getElementById('overlay-root')
+            )}
+        </React.Fragment>
     )
 }
 
